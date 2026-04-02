96c67fc5fdc06ffe257c3e3f082d7ce215d30363
roronan13 <ronan.pothier@gmail.com>
2026-04-02 19:58:22 +0200

    dacc

--- ex0/ft_command_quest.py
+++ ex0/ft_command_quest.py
@@ -1,2 +1,18 @@
 #!usr/bin/env python3
 
+import sys
+
+if __name__ == "__main__":
+    print(" === Command Quest === \n")
+    print(f"Program name : {sys.argv[0]}")
+
+    if len(sys.argv) == 1:
+        print("No arguments provided !")
+    else:
+        print(f"Arguments received : {len(sys.argv) - 1}")
+
+    i: int = 1
+    for i in range(i, len(sys.argv)):
+        print(f"Argument {i} : {sys.argv[i]}")
+
+    print(f"Total arguments : {len(sys.argv)}")
