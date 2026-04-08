265c4dc7ba85d346a151699676725a5234dfac90
roronan13 <ronan.pothier@gmail.com>
2026-04-08 13:17:00 +0200

    pas mal

--- ex4/ft_inventory_system.py
+++ ex4/ft_inventory_system.py
@@ -8,6 +8,14 @@ class DoublonError(Exception):
         super().__init__(error_message)
 
 
+def get_percentage(value: int, total: int) -> float:
+    if total == 0:
+        return 0
+
+    percentage: float = (value * 100) / total
+    return round(percentage, 1)
+
+
 if __name__ == "__main__":
     print(" === Inventory System Analysis === \n")
 
@@ -41,6 +49,19 @@ ft_inventory_system.py item_name:quantity item_name:quantity\n")
             print(f"Quantity error for '{item_and_value[0]}' : {e}")
             continue
 
-        inventory[item_and_value[0]] = item_and_value[1]
+        inventory[item_and_value[0]] = int(item_and_value[1])
 
-    print(inventory)
+    print(f"Got inventory : {inventory}")
+    print(f"Item list : {list(inventory.keys())}")
+    total: int = sum(inventory.values())
+    print(f"Total quantity of the items : {total}")
+
+    for key, value in inventory.items():
+        print(f"Item {key} represents {get_percentage(value, total)}%")
+
+    most_key: str = max(inventory, key=inventory.get)
+    most_value: int = inventory[most_key]
+    print(f"\nItem most abundant : {most_key} with quantity {most_value}")
+    min_key: str = min(inventory, key=inventory.get)
+    min_value: int = inventory[min_key]
+    print(f"Item least abundant : {min_key} with quantity {min_value}")
